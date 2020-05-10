75f13c96352ccb7b7f425a8b194f99b2f0ebd815
Dillon McGovern <dillonm800@gmail.com>
2020-05-10 18:50:43 -0400

    .

--- app.py
+++ app.py
@@ -86,7 +86,8 @@ def function():
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-    return 'server is working'
+    driver.get('https://www.google.com')
+    return driver.page_source
 
 @app.route('/scrape', methods=['GET'])
 def scrape():
