6d228c59c846c47f2b52ef504aad16181e9893b9
Dillon McGovern <dillonm800@gmail.com>
2020-05-10 19:06:11 -0400

    .

--- app.py
+++ app.py
@@ -81,11 +81,15 @@ def function():
 
     chrome_options = webdriver.ChromeOptions();
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    '''
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    '''
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     browser.get('https://www.publix.com/savings/all-deals/meat')
+
+    return browser.page_source
 
     choose_store = browser.find_elements_by_xpath('//*[@id="main"]/div[4]/div[2]/div/div/button')[0]
     choose_store.click()
