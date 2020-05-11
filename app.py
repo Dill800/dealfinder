62e2189772a3d99a908549f66ca975067095ad87
Dillon McGovern <dillonm800@gmail.com>
2020-05-10 20:03:50 -0400

    .

--- app.py
+++ app.py
@@ -78,27 +78,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def function():
-
-    '''
-    chrome_options = webdriver.ChromeOptions();
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-    driver.get('https://www.google.com')
-    return driver.page_source
-    '''
-    '''
+    
     chrome_options = webdriver.ChromeOptions();
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") 
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    '''
-    browser = webdriver.Chrome()
+    
+    # browser = webdriver.Chrome()
     browser.get('https://www.publix.com/savings/all-deals/meat')
 
     time.sleep(2)
