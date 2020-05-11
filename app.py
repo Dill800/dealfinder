b3fe2cb1c79cfd1a42b95563184db7cdf38dc6f9
Dillon McGovern <dillonm800@gmail.com>
2020-05-10 22:29:36 -0400

    deployed version works

--- app.py
+++ app.py
@@ -12,66 +12,6 @@ import os
 
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
-'''
-
-print('== Starting... ==')
-browser = webdriver.Chrome()
-browser.get('https://www.publix.com/savings/all-deals/meat')
-choose_store = browser.find_elements_by_xpath('//*[@id="main"]/div[4]/div[2]/div/div/button')[0]
-choose_store.click()
-
-time.sleep(1)
-
-type_store = browser.find_elements_by_xpath('//*[@id="body-wrapper"]/div[2]/div/div/div[2]/div[1]/form/div[1]/div/input')[0]
-type_store.send_keys('32612')
-time.sleep(1)
-type_store.send_keys(Keys.ENTER)
-
-# wait for response
-time.sleep(2)
-
-village_market = browser.find_elements_by_xpath('//*[@id="body-wrapper"]/div[2]/div/div/div[2]/div[2]/div/ul/li[2]/div/button')[0]
-village_market.click();
-time.sleep(1)
-browser.get('https://www.publix.com/savings/all-deals/meat')
-time.sleep(1)
-html = browser.page_source
-soup = BeautifulSoup(html, 'html.parser')
-
-all = ''
-
-deal_container = soup.find_all('div', class_='text-block-primary card-title clamp-2')
-
-for deal in deal_container:
-    all += deal.text
-    all += '\n'
-
-browser.get('https://www.publix.com/savings/all-deals/produce')
-time.sleep(1)
-html = browser.page_source
-soup = BeautifulSoup(html, 'html.parser')
-
-deal_container = soup.find_all('div', class_='text-block-primary card-title clamp-2')
-
-for deal in deal_container:
-    all += deal.text
-    all += '\n'
-
-print(all)
-'''
-'''
-client = Client(os.environ['ACCOUNT_SID'] or config.values['account_sid'], os.environ['AUTH_TOKEN'] or config.values['auth_token'])
-
-
-client.messages.create(
-    to=os.environ['MY_PHONE'] or config.values['my_phone'],
-    from_=os.environ['TWILIO_PHONE'] or config.values['twilio_phone'],
-    body=all
-)
-'''
-
-
-
 
 
 app = Flask(__name__)
@@ -90,7 +30,6 @@ def function():
     browser.get('https://www.publix.com/savings/all-deals/meat')
 
     time.sleep(1)
-    
     
     orig_source = browser.page_source
 
@@ -125,8 +64,6 @@ def function():
             all += '\n'
         
         browser.close()
-        # OK SO IT ALL WORKS UP TO HERE..... IF WE CAN GET LUCKY WITH THE DEPLOY LOCATION
-        # INFO IS STORED IN ALL VAR
 
         return orig_source + '\n' + all
     except:
@@ -138,16 +75,6 @@ def test():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    '''
-    #fetch the message
-    msg = request.form.get('Body')
-    # create reply
-    resp = MessagingResponse()
-    answer = getCategory(msg)
-    #print(str(answer))
-    #default timeout is 15 seconds
-    resp.message("You said {}".format(msg))
-    '''
 
     msg = request.form.get('Body')
     answer = getCategory(msg)
@@ -161,13 +88,6 @@ def sms_reply():
         from_=os.environ['TWILIO_PHONE'],
         body=answer
     )
-
-    '''
-    f = open("stuff.txt", "a")
-    f.write(answer)
-    print(answer)
-    f.close()
-    '''
 
     return "done loading"
 
@@ -223,24 +143,6 @@ def getCategory(category):
     browser.close()
 
     return all
-
-
-@app.route('/scrape', methods=['GET'])
-def scrape():
-    print('== This is where we start scraping ==')
-
-    response = get('https://www.publix.com/savings/all-deals/meat')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    print(soup)
-
-    deal_container = soup.find_all('div', class_='deal-count')
-
-    for deal in deal_container:
-        print("***************")
-        print(deal)
-
-    return 'success!'
 
 if __name__ == '__main__':
     app.run(debug=True)
