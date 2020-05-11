8b2b90f7999c1907c14447793ac6fd894a70b6a5
Dillon McGovern <dillonm800@gmail.com>
2020-05-10 22:08:11 -0400

    .

--- app.py
+++ app.py
@@ -173,10 +173,12 @@ def sms_reply():
 
 def getCategory(category):
 
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.managed_default_content_settings.images": 2}
-    chrome_options.add_experimental_option("prefs", prefs)
-    browser = webdriver.Chrome(chrome_options=chrome_options)
+    chrome_options = webdriver.ChromeOptions();
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") 
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     browser.get('https://www.publix.com/savings/all-deals/' + category)
 
